8d418650535f66e9f1f5563e29fcdf75a5c76537
Chris Huxtable <chris@huxtable.ca>
2016-02-05 12:59:37 -0500

    Removed unnecessary js

--- r/j/viewer.js
+++
@@ -1,8 +0,0 @@
-function focusOnImage(sender) {
-	sender.parentNode.parentNode.lastElementChild.style.backgroundImage = 'url(' + sender.src + ')';
-}
-
-var elements = document.querySelectorAll(".viewer .strip img.trigger");
-var count = elements.length;
-
-for (var i = 0; i < count; i++) { elements[i].onmouseover = function() { focusOnImage(this); }; }
